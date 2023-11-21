3167601b119625d78b686e772020b89870d74e7d
manojvasava <manojvasava5uk@gmail.com>
2023-11-21 12:12:47 +0000

    add index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,40 @@
 puts "Wiping all players from the database..."
 
-Player.destroy_all
+# Player.destroy_all
 
-puts "Generating 35 new players..."
+# puts "Generating 35 new players..."
 
-35.times do
-  name = Faker::Sports::Football.player
+# 35.times do
+#   name = Faker::Sports::Football.player
+#   description = "#{Faker::Sports::Football.position}. #{Faker::Quote.mitch_hedberg}"
+#   address = Faker::Address.full_address
+#   gender = %w[male female].sample
+#   Player.create(name: name, description: description, address: address, gender: gender)
+# end
+
+puts "Game generation Start."
+
+10.times do
   description = "#{Faker::Sports::Football.position}. #{Faker::Quote.mitch_hedberg}"
   address = Faker::Address.full_address
   gender = %w[male female].sample
-  Player.create(name: name, description: description, address: address, gender: gender)
+  team_size = 12
+  pitch_identifier = "pitch number"
+  pitch_type =   %w[male female].sample
+  starting_date =
+  ending_date =
+  recurring_rule =
+  Games.create(
+    description: description,
+    address: address,
+    gender: gender,
+    team_size: team_size,
+    pitch_identifier: pitch_identifier,
+    pitch_type: pitch_type,
+    starting_date: starting_date,
+    ending_date: ending_date,
+    description: description,
+    recurring_rule: recurring_rule,
+  )
 end
-
-puts "Player generation complete."
+puts "Game generation complete."

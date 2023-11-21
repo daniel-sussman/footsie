af50101402afd2752271c39b6ff2c8841616e53e
Daniel Sussman <daniel.o.sussman@gmail.com>
2023-11-21 18:02:47 +0000

    Navbar functional

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,7 +1,11 @@
 class PlayersController < ApplicationController
   def show
-    @player = Player.find(params[:id])
-    @player_games = PlayerGame.where("player_id = #{@player.id} AND active = true")
+    if params[:id] == "sign_out"
+      redirect_to games_path
+    else
+      @player = Player.find(params[:id])
+      @player_games = PlayerGame.where("player_id = #{@player.id} AND active = true")
+    end
   end
 
   def new
@@ -11,7 +15,7 @@ class PlayersController < ApplicationController
   def create
     @player = Player.new(player_params)
     if @player.save
-      redirect_do games_path
+      redirect_to games_path
     else
       render :games, status: :unprocessable_entity
     end

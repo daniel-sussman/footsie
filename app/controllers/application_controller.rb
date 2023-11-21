6cfa2534e59af81391c42e32454230e4f071019b
Daniel Sussman <daniel.o.sussman@gmail.com>
2023-11-21 11:56:59 +0000

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
           def configure_permitted_parameters
                devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :description, :address, :gender, :email, :password)}
 
-               devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:description, :address, :gender, :email, :password, :current_password)}
+               devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :description, :address, :gender, :email, :password, :current_password)}
           end
 
 end

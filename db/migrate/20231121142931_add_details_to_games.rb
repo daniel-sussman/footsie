bb24af3fc980cb8cdea988a6a041d645580f4e3a
Daniel Sussman <daniel.o.sussman@gmail.com>
2023-11-22 12:38:28 +0000

    Unnecessary migration

--- db/migrate/20231121142931_add_details_to_games.rb
+++
@@ -1,7 +0,0 @@
-class AddDetailsToGames < ActiveRecord::Migration[7.1]
-  def change
-    add_column :games, :name, :string
-    add_column :games, :indoor, :boolean
-    add_column :games, :price, :float
-  end
-end
